7a292d6fda1aef95dddf34a25fb000ca620435e1
DhanrajMV <86152041+DhanrajMV@users.noreply.github.com>
2022-06-02 14:36:05 +0530

    added ERP project

---
+++ ERP/src/main/java/genericLibrary/IAutoConstants.java
@@ -0,0 +1,12 @@
+package genericLibrary;
+/***
+ * 
+ * @author Sumanth
+ *
+ */
+public interface  IAutoConstants {
+	
+	String CHROME_KEY = "webdriver.chrome.driver";
+	String CHROME_PATH = "C:\\Users\\DevSys002\\eclipse-workspace\\New folder\\ERP\\Drivers\\chromedriver.exe";
+		
+}
